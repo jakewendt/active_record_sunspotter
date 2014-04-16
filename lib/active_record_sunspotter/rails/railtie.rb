9ea1f5efad03047e0895648772f61587153c7ea0
Jake <github@jakewendt.com>
2014-04-16 14:55:34 -0700

    Corrected the sunspot helper inclusion.

--- lib/active_record_sunspotter/rails/railtie.rb
+++ lib/active_record_sunspotter/rails/railtie.rb
@@ -3,7 +3,10 @@ module ActiveRecordSunspotter
 		initializer 'add_helpers_to_actionview' do |app|
 			ActiveSupport.on_load :action_view do
 				ActionView::Base.send(:include, ActiveRecordSunspotter::SunspotHelper)
-				ActionController::Base.helper :sunspot
+				#	:sunspot would require sunspot_helper.rb be in one of the magical load paths.
+				#ActionController::Base.helper :sunspot
+				#	So much simpler to just pass the module name.
+				ActionController::Base.helper ActiveRecordSunspotter::SunspotHelper
 			end
 		end
 #		rake_tasks do
